c126b73c4de3558952c6802dc1cdbb983a1d78da
Erin  Rose <errose10@gmail.com>
2020-01-01 16:42:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -61,7 +61,7 @@ class Song
   end
 
   def self.create_from_filename(filename)
-    song = self.new_from_filename(fileman)
+    song = self.new_from_filename(filename)
     @@all << song
     song
   end
