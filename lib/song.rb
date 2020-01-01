b1ceb4da44ac2ef74f70c166554e97658723198b
Erin  Rose <errose10@gmail.com>
2020-01-01 16:10:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,6 +40,10 @@ class Song
   end
   def self.find_or_create_by_name(name)
     song = self.find_by_name(name)
-    binding.pry
-  end
+      if song == nil 
+        then song = self.create_by_name(name)
+      end
+      song
+    end
+    
 end
